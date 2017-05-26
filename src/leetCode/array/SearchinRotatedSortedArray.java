7b583125e6ffd7b272053e6fc0b41aaaa9d659c3
Chu2015 <1198209488@qq.com>
2017-05-26 08:16:37 +0800

    update

--- src/leetCode/array/SearchinRotatedSortedArray.java
+++ src/leetCode/array/SearchinRotatedSortedArray.java
@@ -3,13 +3,38 @@ package leetCode.array;
 public class SearchinRotatedSortedArray {
 	
     public int search(int[] nums, int target) {
-    	    	
+    	if(nums == null){
+    		return -1;
+    	}    
+    	
     	int len = nums.length;
+    	int mid = (len-1)/2;
+    	int start = 0;
+    	int end = len-1;
     	
-    	if(nums[(len-1)/2] > nums[0])
-    	
+    	while(start < end){
+    		
+	    	if(nums[mid] > nums[start]){
+	    		if(target < nums[(len-1)/2] && target > nums[start]){
+	    			end = mid-1;
+	    			continue;
+	    		}else if(target == nums[(len-1)/2]){
+	    			return mid;
+	    		}else{
+	    			//µİ¹é
+	    		}
+	    	}else if(nums[mid] < nums[start]){
+	    		
+	    	}
+	    	
+    	}
     	
     	
         return 0;
     }
+    
+    public int search(int[] nums,int start,int end){
+    	return 0;
+    }
+    
 }
