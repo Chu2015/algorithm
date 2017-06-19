b479adaf9b502a1ce6f65328e328b11c83615ea9
Chu2015 <1198209488@qq.com>
2017-06-19 19:49:50 +0800

    update

--- src/jianzhiOffer/IsPopOrder.java
+++ src/jianzhiOffer/IsPopOrder.java
@@ -1,9 +1,37 @@
 package jianzhiOffer;
 
+import java.util.HashMap;
+
 public class IsPopOrder {
 	
     public boolean IsPopOrder(int [] pushA,int [] popA) {
+    	HashMap<Integer,Integer> map = new HashMap();
+    	int [] popB = new int[popA.length];
+    	for(int i=0;i<pushA.length;i++){
+    		map.put(pushA[i], i+1);
+    	}
+    	for(int i=0;i<popA.length;i++){
+    		popB[i] = map.get(popA[i]);
+    	}
+    	for(int i=0;i<popA.length;i++){
+    		for(int j=i+1;j<popA.length;j++){
+        		if(popA[j]>popA[i]){
+        			continue;
+        		}else{
+        			
+        		}
+        	}
+    	}
+    	
+    	
+    	
+    	
+    	
+    	
+    	
 		return false;
         
     }
+    
+    
 }
