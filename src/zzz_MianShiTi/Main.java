2ec940ab679fdd0ef758fccf05872941ea596a09
Chu2015 <1198209488@qq.com>
2017-08-07 19:28:21 +0800

    update

--- src/zzz_MianShiTi/Main.java
+++ src/zzz_MianShiTi/Main.java
@@ -1,5 +1,26 @@
 package zzz_MianShiTi;
 
-public class Main {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Main {
+	 public static void main(String[] args) {
+		   Scanner input = new Scanner(System.in);
+		   int n=0;
+		   int[] f = new int [50];
+		   f[0]=0;
+		   f[1]=1;
+		   
+	       while (input.hasNext()) {
+	    	   n = input.nextInt();
+	    	   int i;
+	    	   for(i=2;n>f[i];i++){
+	    		   f[i] = f[i-1]+f[i-2];
+	    	   }
+	    	   if(n==f[i]){
+	    		   System.out.println(0);
+	    	   }
+	    	   System.out.println(f[i]-n>n-f[i-1] ? f[i]-n : n-f[i-1]);
+	       }
+	  }
 }
