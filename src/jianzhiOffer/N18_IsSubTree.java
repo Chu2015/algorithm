370ad165841d2e8fe41dd6bc599f070f65b565e7
Chu Pengcheng <1198209488@qq.com>
2022-06-08 23:20:57 +0800

    feat: src/jianzhiOffer/N18_IsSubTree.java

--- src/jianzhiOffer/N18_IsSubTree.java
+++ src/jianzhiOffer/N18_IsSubTree.java
@@ -13,7 +13,7 @@ public class N18_IsSubTree {
     	Stack<TreeNode> s = new Stack<TreeNode>();
     	while(!s.isEmpty()|| root1!=null){
 			while(root1!=null){
-				if(isSameTree(root1,root2)){
+				if(subHasSubTreeContainsRoot(root1,root2)){
 					return true;
 				}
 				s.push(root1);
@@ -24,44 +24,34 @@ public class N18_IsSubTree {
     	}	
 		return false;
     }
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-    	//if q is this far,than is true at this root
-    	if(q==null){
-		return true;
-    	}
-		if(p==null){
+
+
+	//dicide if root2 is subtree of root1(both could be null)
+	public boolean HasSubtreeRecursively(TreeNode root1,TreeNode root2) {
+		if(root1==null || root2==null){
 			return false;
 		}
-    	return (p.val == q.val) && isSameTree(p.right,q.right) &&isSameTree(p.left,q.left);
-    }
-    //dicide if root2 is subtree of root1(both could be null)
-    public boolean HasSubtree2(TreeNode root1,TreeNode root2) {
-    	//确保进入迭代方法的是两个不为空的树
-    	if(root2==null || root1==null){
-    		return false;
-    	}
-    	return subHasSubTree(root1,root2);
-    }
-    
-    //  dicide if root2 is subtree of root1( two not-null binaryTree)
-    private boolean subHasSubTree(TreeNode root1,TreeNode root2){
-    	//边界条件的处理，如果root2已经比到空的地步，说明前面的都相同，已经成功
-    	if(root2==null){
-    		return true;
-    	}
-    	//root2不为空，root1为空必然失败
-    	if(root1==null){
-    		return false;
-    	}
-    	//第一处递归，针对当前结点，如果当前结点tree2和tree1值相同，再分别比较他们左子树和右子树
-    	if(root1.val==root2.val && subHasSubTree(root1.left,root2.left) && subHasSubTree(root1.right ,root2.right)){
-    		return true;
-    	}
-    	//第二处递归，针对每个结点遍历，这里是先序遍历
-		return subHasSubTree(root1.left,root2) || subHasSubTree(root1.right,root2);
-    }
-    
-    public static void main(String[] args){
+
+		//第二处递归，针对每个结点遍历，这里是先序遍历
+		return subHasSubTreeContainsRoot(root1, root2) || HasSubtreeRecursively(root1.left,root2) || HasSubtreeRecursively(root1.right,root2);
+	}
+
+	private boolean subHasSubTreeContainsRoot(TreeNode treeNode1, TreeNode treeNode2) {
+        //边界条件的处理，如果root2已经比到空的地步，说明前面的都相同，已经成功
+        if(treeNode2==null){
+            return true;
+        }
+        //root2不为空，root1为空必然失败
+        if(treeNode1==null){
+            return false;
+        }
+        return treeNode1.val == treeNode2.val
+                &&subHasSubTreeContainsRoot(treeNode1.left, treeNode2.left)
+                && subHasSubTreeContainsRoot(treeNode1.right, treeNode2.right);
+	}
+
+
+	public static void main(String[] args){
     	N18_IsSubTree a = new N18_IsSubTree();
     	
     	TreeNode root1 = new TreeNode(8);
@@ -76,6 +66,6 @@ public class N18_IsSubTree {
     	root2.left = new TreeNode(9);
     	root2.right = new TreeNode(2);
     	
-    	System.out.println(a.HasSubtree(root1, root2));
+    	System.out.println(a.HasSubtreeRecursively(root1, root2));
     }
 }
